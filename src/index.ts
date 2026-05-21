0eebc923c93c21c1444732e28d97487c15917395
taha <mtahaamin.official@gmail.com>
2026-05-21 20:29:26 +0500

    Made a skeleton of all possible actions this cli will support via cmdline args

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-console.log('Hello World');
+import { argv } from 'process';
+
+const command = argv.slice(2);
+const action = command[0];
+const args = command.slice(1);
+
+switch (action) {
+  case 'add':
+    break;
+
+  case 'update':
+    break;
+
+  case 'delete':
+    break;
+
+  case 'mark-in-progress':
+    break;
+
+  case 'mark-done':
+    break;
+
+  case 'list':
+    break;
+
+  default:
+    break;
+}
