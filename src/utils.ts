be0f057ff217973d292407b8d4f68ca28e958c39
taha <mtahaamin.official@gmail.com>
2026-05-21 20:25:56 +0500

    Wrote some common utility functions for reading and writing tasks to tasks.json file

---
+++ src/utils.ts
@@ -0,0 +1,20 @@
+import fs from 'fs';
+import path from 'path';
+import type { Task } from './types.js';
+
+const __dirname = import.meta.dirname;
+const TASKS_PATH = path.join(__dirname, './tasks.json');
+
+export const readTasks = (): Array<Task> => {
+  try {
+    let data = fs.readFileSync(TASKS_PATH, 'utf-8');
+    return JSON.parse(data);
+  } catch (error) {
+    console.log('File not found');
+    process.exit(1);
+  }
+};
+
+export const writeTasks = (updatedData: Array<Task>) => {
+  fs.writeFileSync(TASKS_PATH, JSON.stringify(updatedData));
+};
