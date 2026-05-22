b6fe32042c96c2663686d21942b0f34e30661e03
taha <mtahaamin.official@gmail.com>
2026-05-22 17:39:32 +0500

    added a utility function for checking arguments length

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,13 @@ const command = argv.slice(2);
 const action = command[0];
 const args = command.slice(1);
 
+const checkArgs = (required: number) => {
+  if (args.length < required) {
+    console.log('Error: Not enough arguments given for', action?.toUpperCase());
+    process.exit(1);
+  }
+};
+
 switch (action) {
   case 'add':
     break;
