3b9b64dfbf0869eda853428e0a91abc14017bd58
taha <mtahaamin.official@gmail.com>
2026-05-22 18:09:35 +0500

    added function for add task action

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,25 @@
 import { argv } from 'process';
+import { readTasks, writeTasks } from './utils.js';
+import type { Task } from './types.js';
 
 const command = argv.slice(2);
 const action = command[0];
 const args = command.slice(1);
+
+const addTask = (taskDescription: string) => {
+  let tasks = readTasks();
+  const currentDate = new Date();
+  let task: Task = {
+    id: tasks.length + 1,
+    description: taskDescription,
+    status: 'todo',
+    createdAt: currentDate,
+    updatedAt: currentDate,
+  };
+  tasks.push(task);
+  writeTasks(tasks);
+  return task.id;
+};
 
 const checkArgs = (required: number) => {
   if (args.length < required) {
@@ -13,6 +30,9 @@ const checkArgs = (required: number) => {
 
 switch (action) {
   case 'add':
+    checkArgs(1);
+    const taskID = addTask(args[0] as string);
+    console.log(`Task added successfully (ID: ${taskID})`);
     break;
 
   case 'update':
