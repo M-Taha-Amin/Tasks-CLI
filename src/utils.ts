4043577da8caad414e75239d755b66e2225678e1
taha <mtahaamin.official@gmail.com>
2026-05-22 18:50:36 +0500

    added a utility for showing all possible operations on CLI

--- src/utils.ts
+++ src/utils.ts
@@ -30,3 +30,30 @@ export const writeTasks = (updatedData: Array<Task>) => {
     process.exit(1);
   }
 };
+
+export const showHelp = () => {
+  console.log('Task Tracker CLI v1');
+  console.log('Supported operations:');
+
+  console.log('\t Command: add <task_name>');
+  console.log('\t Description: adds a new task with given name to the list\n');
+
+  console.log('\t Command: update <task_id> <new_name>');
+  console.log('\t Description: updates existing task with new name given\n');
+
+  console.log('\t Command: delete <task_id>');
+  console.log('\t Description: deletes the task with given id\n');
+
+  console.log('\t Command: mark-in-progress <task_id>');
+  console.log(
+    '\t Description: marks the status as in-progress of task with given id\n',
+  );
+
+  console.log('\t Command: mark-done <task_id>');
+  console.log('\t Description: marks the task with given id as done\n');
+
+  console.log('\t Command: list <status>');
+  console.log(
+    '\t Description: Lists tasks based on status as a filter, status is optional.\n\t If status is omitted, all tasks are shown\n',
+  );
+};
